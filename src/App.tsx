bb26f90c02f34077447a669cc7cd2ea366765dd1
Elias Atalay <selasie7@yahoo.com>
2023-02-05 00:47:04 +0100

    firstComit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
+import Grid from './components/Grid';
+const inputfile = require('./input.txt')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetchData = async () => {
+  let resp = await axios.get(inputfile)
+  let final = await resp.data
+  var lines = final.split("\r\n");
+  var cols = lines[0].split(" ")[1]
+  var rows = lines[1].split(" ")[1]
+  var generation = lines[2].split(" ")[1]
+
+  var result = {
+    horizontal: cols,
+    vertical: rows,
+    generation: generation
+  };
+  return result
 }
 
-export default App;
+function App() {
+  const [props, setProps] = useState({ vertical: '', horizontal: '', generation: '' })
+  const [grid, setGrid] = useState(false)
+
+  //read the initial setting (generation, grid size) of the game
+  useEffect(() => {
+    const data = fetchData();
+    data.then((res) => {
+      console.log("data", data)
+      console.log("res", res)
+      setProps(res)
+      setGrid(true)
+    })
+
+  }, [])
+  
+  const { vertical, horizontal, generation } = props
+
+  if (grid && ((Number(vertical) * Number(horizontal)) >= Number(generation))) return (<Grid {...props} />)
+  else if (grid && (Number(vertical) * Number(horizontal) < Number(generation))) return (<> <div style={{ margin: "30px" }}>Initial generation is out of grid </div> </>)
+  else return (<> <div style={{ margin: "30px" }}> Initial setting not loaded correctly </div> </>)
+}
+
+export default App
---
+++ src/components/Grid.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, useState } from "react"
+import "../App.css"
+
+//construct the Grid based on the input dimensions and generation number
+const constructGrid = (rows: number, cols: number, gen: number) => {
+
+    const grid = []
+    for (let i = 0; i < rows; i++) {
+        const row = []
+        for (let j = 0; j < cols; j++) {
+            row.push(0)
+        }
+        grid.push(row)
+    }
+    let checkDuplicate: string[] = []
+    for (let k = 0; k < gen; k++) {
+        let rand1 = Math.floor(Math.random() * (rows))
+        let rand2 = Math.floor(Math.random() * (cols))
+        var concatRand = `${rand1}${rand2}`
+        //check if cell is alive
+        if (!checkDuplicate.includes(concatRand)) {
+            checkDuplicate.push(concatRand)
+            grid[rand1][rand2] = 1
+        }
+        //remove cell alive if the grid cell is alive
+        else if (checkDuplicate.includes(concatRand))
+            k--
+    }
+    return grid
+}
+
+// positions: N, S, E, W, NE, NW, SE, SW
+const positions = [
+    [0, 1], // right
+    [0, -1], // left
+    [1, -1], // top left
+    [-1, 1], // top right
+    [1, 1], // top
+    [-1, -1], // bottom
+    [1, 0], // bottom right
+    [-1, 0], // bottom left
+]
+
+function Grid({ ...props }) {
+    const { generation, horizontal, vertical } = props
+    debugger
+    const [grid, setGrid] = useState(() => {
+        return constructGrid(0, 0, 0);
+    })
+    const [start, setStart] = useState(false)
+    const startRef = useRef(start)
+    useEffect(() => {
+        setGrid(constructGrid(horizontal, vertical, generation))
+    }, [])
+
+    console.log("grid grid", grid)
+
+    function runSimulation() {
+        if (!startRef.current) {
+            return
+        }
+        setGrid((grid) => {
+            console.log("grid G", grid)
+            const nextGeneration = grid.map((row, i) => {
+                return row.map((cell, j) => {
+                    let neighbors = 0
+                    positions.forEach((position) => {
+                        const x = i + position[0]
+                        const y = j + position[1]
+                        if (x >= 0 && x < horizontal && y >= 0 && y < vertical) {
+                            neighbors += grid[x][y]
+                        }
+                    })
+                    if (neighbors < 2 || neighbors > 3) {
+                        return 0
+                    }
+                    if (neighbors === 3) {
+                        return 1
+                    }
+                    return grid[i][j]
+                })
+            })
+            return nextGeneration
+        })
+    }
+
+    return (
+        <>
+            <div className="containerGame"> GAME OF LIFE
+                <div className="menuGrid">
+                    <button className="playButton"
+                        onClick={() => {
+                            setStart(!start)
+                            if (!start) {
+                                startRef.current = true
+                            }
+                            setInterval(() => {
+                                runSimulation()
+                            }, 1000)
+                        }}                >
+                        {start ? "Stop" : "Play"}
+                    </button>
+                    <button className="resetButton"
+                        onClick={() => setGrid(constructGrid(horizontal, vertical, generation))}
+                    >
+                        Reset
+                    </button>
+                </div>
+                <div className="grid">
+                    {grid &&
+                        grid.map((rows, i) =>
+                            <li key={i} style={{ listStyle: 'none', margin: "10px" }}>{
+                                rows.map((col, k) => (
+                                    <div key={k} style={{ fontSize: "20px", fontWeight: "bold" }}>{grid[i][k] ? '*' : '.'}</div>
+                                ))}
+                            </li>
+                        )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Grid
